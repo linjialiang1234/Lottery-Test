853de0d12f1a908936c722e03ee47c4e98132fe8
linjialiang1234 <linjialiang86@163.com>
2018-01-01 17:33:47 +1100

    opacity animation

--- container.js
+++ container.js
@@ -84,13 +84,21 @@ function containerController($scope, $element, $attrs) {
                       .data(pie(dataset))
                       .enter()  
                       .append("g")  
-                      .attr("transform","translate("+outerRadius+","+outerRadius+")");  
+                      .attr("transform","translate("+outerRadius+","+outerRadius+")")
+                      .on('mouseover', function(d) {
+                          d3.select(this)
+                          .style('opacity', .5)
+                      })
+                      .on('mouseout', function(d) {
+                          d3.select(this)
+                            .style('opacity', 1);
+                      });  
                         
         arcs.append("path")  
             .attr("fill",function(d,i){  
                 // return color(i);
                 return ctrl.inputLists[i].color;
-            })  
+            })
             .attr("d",function(d){  
                 return arc(d);  
             });  
@@ -108,62 +116,7 @@ function containerController($scope, $element, $attrs) {
         console.log(pie(dataset));  
             
     }
-    // ctrl.drawPieChart = function() {
-    //     var num = ctrl.inputLists.length;
-    //     console.log("num " + num);
-    //     console.log("abc: " + ctrl.inputLists[0].color);
-    //     var canvas = document.getElementById('canvas');
-    //     var btn = document.getElementById('btn');
-    //     if(!canvas.getContext){
-    //         alert('Sorry, this browser do not support it.');
-    //         return;
-    //     }
-
-    //     console.log("canvas" + canvas);
-    //     console.log("btn" + btn);
-
-    //     totolRotateNumber = 0;
-
-    //     var ctx = canvas.getContext('2d');
-    //     for (var i = 0; i < num; i++) {
-    //         // save the current state
-    //         ctx.save();
-    //         // start to draw
-    //         ctx.beginPath();
-    //         // the center has been changed
-    //         ctx.translate(150, 150);
-    //         // from(0, 0) define a new route
-    //         ctx.moveTo(0, 0);
-    //         // uese formular degrees * Math.PI/180 to change degree into arc
-    //         // ctx.rotate((360 / num * i + 360 / num / 2) * Math.PI/180);
-    //         ctx.rotate((360 * (totolRotateNumber/ctrl.getAllInputPercentage())) * Math.PI/180);
-    //         totolRotateNumber += ctrl.inputLists[i].percentage;
-    //         // draw the arc
-    //         // ctx.arc(0, 0, 150, 0,  2 * Math.PI / num, false);
-    //         ctx.arc(0, 0, 150, 0,  2 * Math.PI *(ctrl.inputLists[i].percentage/ctrl.getAllInputPercentage()) , false);
-    //         // if (i % 2 == 0) {
-    //         //     ctx.fillStyle = '#ffb820';
-    //         // }else{
-    //         //     ctx.fillStyle = '#ffcb3f';
-    //         // }
-    //         ctx.fillStyle = ctrl.inputLists[i].color;
-    //         // fill the color
-    //         ctx.fill();
-    //         // draw outline
-    //         ctx.lineWidth = 0.5;
-    //         ctx.strokeStyle = 'whitesmoke';
-    //         ctx.stroke();
-    
-    //         // literal
-    //         // ctx.fillStyle = '#fff';
-    //         // ctx.font="16px sans-serif";
-    //         // ctx.fillText(i + 1, 100, 60);
-    
-    //         //restore previous state
-    //         ctx.restore();
-    //     }
-    // }
-
+  
     var result = 0;
     var counter = 0;
     ctrl.rotatePieChart = function() {
