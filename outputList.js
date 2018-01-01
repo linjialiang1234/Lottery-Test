c116a24ba6b914a3578e2c7899368b8b31ee5364
linjialiang1234 <linjialiang86@163.com>
2018-01-01 15:42:25 +1100

    delete elements

--- outputList.js
+++ outputList.js
@@ -1,9 +1,9 @@
 function outputListController($scope, $element, $attrs) {
     var ctrl = this; 
 
-    ctrl.onRemoveItem = function() {
-        console.log("111:" + this.$index);
-        ctrl.removeItem({index:this.$index});
+    ctrl.onRemoveItem = function($index) {
+        console.log("111:" + $index);
+        ctrl.removeItem({index:$index});
 
     }
 
@@ -17,7 +17,7 @@ angular.module('mainApp').component("outputList",{
             <span>{{list.name}}</span>
             <span>{{list.percentage}}</span>
             <span>{{list.color}}</span>
-            <button ng-click="$ctrl.onRemoveItem()">Delete</button>
+            <button ng-click="$ctrl.onRemoveItem($index)">Delete</button>
         </div>
     </div>
 
